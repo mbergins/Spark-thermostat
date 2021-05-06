610516013f021e115315c6dce343866b73441de3
Matthew Berginski <matthew.berginski@gmail.com>
2021-05-06 13:15:27 -0400

    reenabling BerginskiRMisc, plot formatting changes

--- utilities/plot_temperatures.R
+++ utilities/plot_temperatures.R
@@ -35,7 +35,7 @@ dir.create(args$folder, showWarnings = FALSE)
 library(ggplot2)
 library(lubridate)
 library(dplyr)
-#library(BerginskiRMisc)
+library(BerginskiRMisc)
 library(grid)
 library(scales)
 
@@ -49,26 +49,15 @@ tempPlot = ggplot(temp,aes(x = myTime)) +
   geom_line(aes(y=Target_temp,color = "Target"), alpha = 0.5) +
   geom_line(aes(y=Freezer_temp,color = "Freezer"), alpha = 0.9) +
   geom_line(aes(y=Outside_Temp,color = "Outside"), alpha = 0.9) +
-  ylab('Temperature (°F)') +
+  ylab('Temperature (\u00B0F)') +
   xlab('Time') +
-  #theme_berginski() +
   coord_cartesian(ylim = c(30,100)) +
   scale_x_datetime(date_breaks = "1 day", labels = date_format("%d/%m/%y")) +
   scale_color_manual(values = c("Blue","Red","White","LightBlue","Green","Gray10"),
                      limits = c("Cold","Hot","Neither","Freezer","Outside","Target")) +
   labs(color = "") +
-  theme(text = element_text(size=6),
-        axis.title.x=element_text(margin=margin(1.5,0,0,0)),
-        axis.title.y=element_text(margin=margin(0,1.5,0,0))) +
-  theme(panel.grid = element_blank(),
-        panel.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.ticks = element_line(color='black'),
-        axis.text = element_text(color='black'),
-        axis.title.x=element_text(margin=margin(1.5,0,0,0)),
-        axis.title.y=element_text(margin=margin(0,1.5,0,0)))
-
+  theme_berginski() +
+  theme(text = element_text(size=8))
 
 
 ggsave(file.path(args$folder,'week.jpg'),tempPlot,width=4.25,height=2)
@@ -81,4 +70,4 @@ tempPlotDay = tempPlot %+% tempDay +
   scale_x_datetime(date_breaks = "1 hour", date_labels = '%H')
 
 ggsave(file.path(args$folder, 'day.jpg'), tempPlotDay, width=4.25, height=2)
-#trimImage(file.path(args$folder, 'day.jpg'))
+trimImage(file.path(args$folder, 'day.jpg'))
